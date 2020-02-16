36955355545b6c5b35f1463f7283115577fa56f3
hannameden <hannameden@gmail.com>
2020-02-16 21:19:52 +0100

    important fixes

--- State.java
+++ State.java
@@ -1,15 +1,10 @@
 
-
 public abstract class State {
 
 	private static State currentState;
 
-	public void handleCoinInserted(StateClient stateMachine) {	}
 	public void pointerDown(Point point, ShapeContainer moder) {	}
 	public void pointerMoved(Point point, ShapeContainer shapeContainer, boolean pointerDown) {	}
-
-//	public static void reset(StateClient client) {
-//		setState(new StateA(client));	}
 
 	protected static void setState(State newState) {
 		currentState = newState;
